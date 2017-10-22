bda5955d86e70b20ba2ff1434ab01a07ce5dc8b3
kraus1049 <kraus1049@gmail.com>
2017-10-22 15:20:22 +0900

    write setup_environment.sh

---
+++ setup_environment.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# tex
+if type uplatex >/dev/null 2>&1; then
+	echo "texlive is already installed"
+else
+	sudo apt -y install texlive-full
+	sudo kanji-config-updmap-sys ipaex
+fi
+
+# c
+if type clang >/dev/null 2>&1; then
+	echo "clang is already installed"
+else
+	sudo apt -y install clang clang-format
+fi
+
+#rust
+if type cargo >/dev/null 2>&1; then
+	echo "rust is already installed"
+else
+	curl -L https://static.rust-lang.org/rustup.sh | sudo sh
+fi
+
+#Haskell stack
+if type stack >/dev/null 2>&1; then
+	echo "stack is already installed"
+else
+	curl -sSL https://get.haskellstack.org/ | sh
+fi
